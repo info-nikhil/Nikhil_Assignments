1081d441831acade5f3e8827d2366d085aebeab5
unknown <info.nikhil001@gmail.com>
2026-03-25 00:09:44 +0530

    March 2

---
+++ March2Assignments/Day_39_Projects/CodeFirstDemo/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstDemo.Models
+{
+    class Category
+    {
+        public int Id { set; get; }
+        public string Name { set; get; }
+        public List<Product> Products { set; get; }
+
+    }
+}
