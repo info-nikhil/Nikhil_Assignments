bd8a7d44813bc327efe85090755027e5ef943f6c
Nikhil Kumar <info.nikhil001@gmail.com>
2026-04-02 11:40:39 +0530

    March19Assignments

---
+++ March19Assignments/Day_51_Projects/CodeFIrst_EF_in_ASPNET/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CodeFIrst_EF_in_ASPNET.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+
+        [Required]
+        public string ProductName { get; set; }
+
+        public Customer Customer { get; set; }
+
+        [Display(Name = "who buyed")]
+        [ForeignKey("Customer")]
+        public int CustomerId { get; set; }
+    }
+}
---
+++ March19Assignments/Day_51_Projects/CodeFIrst_EF_in_ASPNET/Repositories/IPost.cs
@@ -0,0 +1,19 @@
+﻿using CodeFIrst_EF_in_ASPNET.Models;
+
+namespace CodeFIrst_EF_in_ASPNET.Repositories
+{
+    public interface IPost
+    {
+        List<Post> GetPosts();
+
+        Post GetPostByID(int postid);
+
+        void InsertPost(Post post);
+
+        void DeletePost(int postid);
+
+        void UpdatePost(Post post);
+
+        void save();
+    }
+}
