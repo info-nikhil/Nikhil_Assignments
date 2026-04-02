ba5fa062d4778d49c777bed9207ed5850ece604e
Nikhil Kumar <info.nikhil001@gmail.com>
2026-04-02 11:33:13 +0530

    March23Assignments

---
+++ March23Assignments/StateManagement/StateManagement/Controllers/AccountController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Antiforgery;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StateManagement.Models;
+
+namespace StateManagement.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                //var cookieOptions = new CookieOptions
+                //{
+                //    Expires = DateTime.Now.AddMinutes(1)
+                //};
+                //Response.Cookies.Append("UserName", model.UserName, cookieOptions);
+
+                HttpContext.Session.SetString("UserName", model.UserName);
+                return RedirectToAction("Welcome");
+            }
+            return View(model);
+        }
+
+        public IActionResult Welcome()
+        {
+            var username = HttpContext.Session.GetString("UserName");
+            //if (Request.Cookies.ContainsKey("UserName"))
+            //{
+            //    string username = Request.Cookies["UserName"];
+            //    ViewBag.UserName = username;
+            //}
+
+            if (!string.IsNullOrEmpty(username))
+            {
+                ViewBag.UserName = username;
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult LogOut()
+        {
+            //Response.Cookies.Delete("UserName"); //For Cookies
+
+
+            //HttpContext.Session.Remove("UserName");  //For Session
+
+
+            HttpContext.Session.Clear();  //Added by me
+            Response.Cookies.Delete(".AspNetCore.Session"); //Added by me
+
+
+
+            return RedirectToAction("Login");
+        }
+    }
+}
